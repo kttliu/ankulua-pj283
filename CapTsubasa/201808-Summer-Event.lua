e39651a36c5b2c87f731d83e49f5c4a01eea975a
kttliu <kttliu@gmail.com>
2018-08-23 22:47:42 +0800

    1) Create Logger module 2) Added Join From Home toggle feature, can join game from stage selection / guild game screen

--- CapTsubasa/201808-Summer-Event.lua
+++ CapTsubasa/201808-Summer-Event.lua
@@ -1,10 +1,16 @@
 --
+--
 -- Created by IntelliJ IDEA.
 -- User: timothy
 -- Date: 17/8/2018
 -- Time: 22:27
 -- To change this template use File | Settings | File Templates.
 --
+
+-- Load logger module
+logger =  require(scriptPath().."module/mod_logger")
+
+-- Global Paramaeters
 setImmersiveMode(true)
 scriptDimension = 1280
 Settings:setCompareDimension(true, scriptDimension)
@@ -26,11 +32,13 @@ regionErrorDialog = Region(266,133,742,444)
 scriptTime = Timer()
 noOfGamesFinished = 0
 
+selected_verbose = "0 - NONE"
 stage =  230
 gameMode = 30
 attackMode = 0
 gameMode = 21
 selected_guild_event = ""
+joinFromHomeSwitch=true
 debugMode=false
 
 --- Display menu
@@ -47,6 +55,12 @@ function mainMenu()
 
     addCheckBox("debugMode", "Enable Log", false)
     addCheckBox("attackModeBoolean", "Attack!", false)
+    addCheckBox("joinFromHomeSwitch","Join From Home", true)
+
+    addSeparator()
+    local spinnerItems = {"0 - NONE", "1 - INFO", "2 - DEBUG", "3 - TRACE" }
+    addTextView("Verbose:  ")
+    addSpinner("selected_verbose", spinnerItems, "0 - NONE")
 
     dialogShow("Menu")
 end
@@ -68,7 +82,7 @@ function menu_join_guild_game()
     if gameMode==21 then
         dialogInit()
         -- Guild Game - Event to join
-        spinnerItems = {"0000 - Any", "0001 - 2018 Summer Event"}
+        local spinnerItems = {"0000 - Any", "0001 - 2018 Summer Event"}
         addTextView("Guild Event To Join:  ")
         addSpinner("selected_guild_event", spinnerItems, "0000 - Any")
 
@@ -86,6 +100,10 @@ else
     attackMode = 0
 end
 
+logger.set(string.sub(selected_guild_event,1,2) + 0)
+
+----- End Menu Processing
+
 
 -- Set Stage picture
 stage_pic = "event-row2-text.png"
@@ -97,38 +115,17 @@ elseif stage==160 then
     stage_pic =   "event-row1-text.png"
 end
 
-function createlogfile()
-    local now = os.time()
-    local now_str = os.date("%c", now)
-
-    local fileName = scriptPath() .. "log.txt"
-    file = io.open(fileName, "w")
-    file:write("LOG Start: " .. now_str, "\n")
-end
-
-function log(msg)
-    toast(msg)
-    write(msg)
-end
-
-function write(msg)
-    if(debugMode) then
-        local now = os.time()
-        local now_str = os.date("%c", now)
-        file:write(now_str .. " : " .. msg, "\n")
-    end
-end
-
 --handle error
 function handleError()
     -- Prevent cap screen again for error checking
     --exists(Pattern("dummy.png"):similar(0.90),0)
     usePreviousSnap(false)
     snapshot()
-    --log("Handle error")
+    logger.debug("Handle error")
     if regionErrorDialog:exists(Pattern("dismissed-text.png"):similar(0.90),0) then
         -- handle dismiss
-        log ("Ooh, dismissed")
+        logger.info ("Ooh, dismissed")
+
         regionCenter:existsClick(Pattern("ok-button.png"):similar(0.90), 30)
         usePreviousSnap(false)
         regionLowerHalf:existsClick(Pattern("confirm-join.png"):similar(0.90), 30)
@@ -137,7 +134,7 @@ function handleError()
     elseif regionErrorDialog:existsClick(Pattern("reconnect.png"):similar(0.90),0) then
         usePreviousSnap(false)
         -- handle disconnected, retry it
-        log ("Ooh, no connection")
+        logger.info ("Ooh, no connection")
         -- retry 3 times
         regionErrorDialogexistsClick(Pattern("reconnect.png"):similar(0.90), 10)
         regionErrorDialogexistsClick(Pattern("reconnect.png"):similar(0.90), 10)
@@ -184,32 +181,32 @@ function find_daily_event()
 end
 
 function joinFromHome()
-    log("Select Normal Game")
+    logger.debug("Select Normal Game")
     if existsClick(Pattern("normal-game.png"):similar(0.90), 50) then
-        log("Select Event Game")
+        logger.debug("Select Event Game")
         if existsClick(Pattern("join-event.png"):similar(0.90), 50) then
-            log("Select 18 Summer Event")
+            logger.debug("Select 18 Summer Event")
             if existsClick(Pattern("18summer-event.png"):similar(0.90), 50) then
-                log("Loop game now")
+                logger.debug("Loop game now")
             end
         end
     end
 end
 
 function joinFromHome_JoinGuild()
-    log("Select Normal Game")
+    logger.info("Select Normal Game")
     regionRightHalf:existsClick(Pattern("normal-game.png"):similar(0.90), 50)
 end
 
 -- Set to attack mode
 -- param: flag  to indicate if last attemp is sucessful
--- return true if cannot deteled that Attack button is ON; otherwise false
+-- return true if cannot deteled (i.e. Attack button is ON); otherwise false
 function setAttackMode(isAttacking)
     if(attackMode==1 and isAttacking==false) then
         if regionInGameAttackModeButtons:existsClick(Pattern("attack-mode-button.png"):similar(0.95),0) then
+            logger.info ("Changing to Attack Mode")
             return false
         end
-        log("Changed to Attack Mode")
         return true
     end
 end
@@ -222,11 +219,11 @@ end
 
 -- To-Do: To be implemeted
 function wait_joiner()
-    log("waiting for joiner")
+    logger.debug("waiting for joiner")
     while true do
         -- To-Do: To implement menu selection later
         if exists(joiners_pic, 1) then
-            log("Enough Joiner")
+            logger.debug("Enough Joiner")
             break
         end
         --avoid screen timeout
@@ -243,9 +240,9 @@ function shootIfGetBallInFrontArea()
 
     if (front_field_area : exists(Pattern(imageToSearch_Ball):similar(0.85), 2)) then
         usePreviousSnap(true)
-        log("Found ball in the front")
+        logger.debug("Found ball in the front")
         front_field_area : existsClick(Pattern(imageToSearch_Shoot):similar(0.95), 1)
-        log("Shoot!!!")
+        logger.debug("Shoot!!!")
     end
     usePreviousSnap(false)
 end
@@ -253,14 +250,14 @@ end
 
 function recurringJoin()
     while(true) do
-        log("Select Stage: " .. stage)
+        logger.info("Select Stage: " .. stage)
         if existsClick(Pattern(stage_pic):similar(0.90), 30) then
-            log("Start Stage")
+            logger.info("Start Stage")
             if existsClick(Pattern("ready-button.png"):similar(0.90), 40) then
-                log("Select Join Public Game")
+                logger.info("Select Join Public Game")
                 if existsClick(Pattern("join-public.png"):similar(0.90), 40) then
                     wait(5)
-                    log("Select Team")
+                    logger.info("Select Team")
                     -- To-Do: Implement select team function
                     if existsClick(Pattern("confirm-join.png"):similar(0.90), 30) then
                         t = Timer()
@@ -269,7 +266,7 @@ function recurringJoin()
                         while(true) do
                             -- To-Do: Long waiting time to restart the game
 
-                            log("# of games finished:" .. noOfGamesFinished .. " wait time: " .. t:check())
+                            logger.info("# of games finished:" .. noOfGamesFinished .. " wait time: " .. t:check())
                             -- Check game started
 
                            -- Shoot in the front
@@ -280,13 +277,13 @@ function recurringJoin()
 
                             -- Check for the complete match screen
                             if exists(Pattern("complete-match-text.png"):similar(0.90), 1) then
-                                log("Click through all screens")
+                                logger.info ("Click through all screens")
                                 while(true) do
                                     if(existsClick(Pattern("finish-match.png"):similar(0.90), 1)) then
                                         noOfGamesFinished = noOfGamesFinished + 1
                                         break;
                                     else
-                                        log("Click ...")
+                                        logger.info ("Click ...")
                                         click(getLastMatch())
                                     end
                                 end
@@ -294,7 +291,7 @@ function recurringJoin()
                                 noOfGamesFinished = noOfGamesFinished + 1
 
                                -- wait long enough for the game menu
-                               log("Completing game and wait for stage menu")
+                                logger.info("Completing game and wait for stage menu")
                                 wait(20)
                                 break;
                             end
@@ -323,13 +320,14 @@ function recurringJoinGuild()
        local counter = 1
         while(true) do
             t = Timer();
-            log("# of games finished:" .. noOfGamesFinished)
+            logger.info("# of games finished:" .. noOfGamesFinished)
+
             -- Go to the Waiting Room if exists
             -- Use last capture image for checking
             --usePreviousSnap(true)
-            write("BEGINE Loop");
+            logger.debug("BEGINE Loop");
             snapshot()
-            write("Snapshot: ".. t:check());
+            logger.trace("Snapshot: ".. t:check());
 
             if regionGuildGameShowArea:existsClick(Pattern(guild_event_to_join()):similar(0.90), 0) then -- Join selected event(s)
             elseif regionLowerHalf:existsClick(Pattern("confirm-join.png"):similar(0.90),0) then
@@ -337,7 +335,7 @@ function recurringJoinGuild()
                 -- usePreviousSnap(false)
 
                -- Joined a game, do in game handling
-                write("confirm-join 1: ".. t:check());
+                logger.trace("confirm-join 1: ".. t:check());
                 local counterInGame = 1
                 while(true) do
 
@@ -346,11 +344,11 @@ function recurringJoinGuild()
 
                     -- Check for the complete match screen
                     -- Disable capture cache
-                    write("In Game - before chcck complete match: ".. t:check());
+                    logger.trace("In Game - before chcck complete match: ".. t:check());
                     usePreviousSnap(false)
                     if (counterInGame%4==0   and regionCenter:exists(Pattern("complete-match-text.png"):similar(0.90),0)) then
                         noOfGamesFinished = noOfGamesFinished + 1
-                        log("Click through all screens")
+                        logger.info("Click through all screens")
 
                         while(true) do
                             click( Location(300, 300))
@@ -359,10 +357,10 @@ function recurringJoinGuild()
                             end
                         end
                         -- wait long enough for the game menu
-                        log("Completing game")
+                        logger.info("Completing game")
                         break;
                     else
-                        write("In Game - Other Start: ".. t:check());
+                        logger.trace("In Game - Other Start: ".. t:check());
                         usePreviousSnap(true)
 
                         -- Set Attack Mode
@@ -372,7 +370,7 @@ function recurringJoinGuild()
                         -- shootIfGetBallInFrontArea()
 
                         -- check error, handle for next game, if any
-                        write("In Game - before handle error: ".. t:check());
+                        logger.trace("In Game - before handle error: ".. t:check());
                         if counterInGame %10 == 0 then
                             if handleError() then
                                 -- Disable capture cache
@@ -380,36 +378,37 @@ function recurringJoinGuild()
                                 break;
                             end
                         end
-                        write("In Game - after handle error: ".. t:check());
+                        logger.trace("In Game - after handle error: ".. t:check());
 
                         -- Disable capture cache
                         usePreviousSnap(false)
-                        write("In Game - Other End: ".. t:check());
+                        logger.trace("In Game - Other End: ".. t:check());
                     end
                     counterInGame = counterInGame + 1
                 end
-                write("confirm-join 2: ".. t:check());
+                logger.trace("confirm-join 2: ".. t:check());
 
                 -- Disable capture cache
                 usePreviousSnap(false)
             elseif regionRightHalf:existsClick(Pattern("join-guild-game.png"):similar(0.90),0) then
             end
 
-            write("Before handle Error: ".. t:check());
+            logger.trace("Before handle Error: ".. t:check());
             if counter%10 == 0 then
                 regionLowerHalf:existsClick(Pattern("finish-match.png"):similar(0.90), 0)
                 handleError()
             end
-            write("After handle Error: ".. t:check());
+            logger.trace("After handle Error: ".. t:check());
             counter = counter + 1
         end
 end
 
 -- main
-createlogfile()
 if gameMode==21 then
+   if(joinFromHomeSwitch) then
     joinFromHome_JoinGuild()
-    recurringJoinGuild()
+   end
+   recurringJoinGuild()
 elseif gameMode==31 then
     joinFromHome()
     recurringJoin();
---
+++ CapTsubasa/module/mod_logger.lua
@@ -0,0 +1,60 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: timothy
+-- Date: 23/8/2018
+-- Time: 0:10
+-- To change this template use File | Settings | File Templates.
+--
+local mod_logger=  {}
+
+------------------------------------
+-- logger mode: Control level of information to log
+-- 0 : None
+-- 1: Info Only
+-- 2: Debug
+-- 3: Trace
+------------------------------------
+local logger_mode = 0
+local now = os.time()
+local now_str = os.date("%c", now)
+
+local fileName = scriptPath() .. "logger.log"
+local file
+file = io.open(fileName, "w")
+file:write("LOG Start: " .. now_str, "\n")
+
+function mod_logger.set(msg)
+    logger_mode = msg
+end
+
+function mod_logger.info(msg)
+    if logger_mode >= 1 then
+        toast(msg)
+    end
+end
+
+function mod_logger.debug(msg)
+    if logger_mode >= 2 then
+        write(msg)
+    end
+end
+
+function mod_logger.trace(msg)
+    if logger_mode >= 3 then
+        write(msg)
+    end
+end
+
+function write(msg)
+    local now = os.time()
+    local now_str = os.date("%c", now)
+
+    if(file == Nil) then
+        init_log_file()
+    end
+    file:write(now_str .. " : " .. msg, "\n")
+end
+
+return mod_logger
+
+
